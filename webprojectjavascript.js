0943ce7623d42b44787189755fc47c133a4b166e
Fatima <154583225+fatima-toto@users.noreply.github.com>
2025-05-01 20:00:32 +0300

    Add files via upload

---
+++ webprojectjavascript.js
@@ -0,0 +1,12 @@
+function welcome() {
+    let a = document.getElementById("inputt").value;
+
+    if (a.trim() != "") {
+        alert(a+" you are welcome");
+
+    }
+}
+function display()
+{
+    confirm("Are You sure for buying?");
+}
